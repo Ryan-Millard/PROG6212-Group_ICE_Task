36e2ad7e6744aa49e07d0c8f2912ea9c3de25851
Ryan-Millard <ryan@Ryans-PC>
2024-08-26 22:22:51 +0200

    Refined layout - looks more like IIE MSA's website

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -36,6 +36,20 @@ namespace EventManagementSystem
 				new Event { Name = "Health Fair", Department = "Health", EventType = "Fair", Date = new DateTime(2024, 10, 18) },
 				new Event { Name = "Career Development Seminar", Department = "Business", EventType = "Seminar", Date = new DateTime(2024, 10, 23) },
 				new Event { Name = "Art Exhibition", Department = "Arts", EventType = "Exhibition", Date = new DateTime(2024, 11, 1) },
+				new Event { Name = "Entrepreneurship Panel", Department = "Business", EventType = "Panel", Date = new DateTime(2024, 11, 10) },
+				new Event { Name = "Science Seminar", Department = "Science", EventType = "Seminar", Date = new DateTime(2024, 9, 1) },
+				new Event { Name = "Arts Workshop", Department = "Arts", EventType = "Workshop", Date = new DateTime(2024, 9, 5) },
+				new Event { Name = "Engineering Conference", Department = "Engineering", EventType = "Conference", Date = new DateTime(2024, 9, 10) },
+				new Event { Name = "Business Social", Department = "Business", EventType = "Social", Date = new DateTime(2024, 9, 15) },
+				new Event { Name = "Technology Expo", Department = "Technology", EventType = "Expo", Date = new DateTime(2024, 9, 20) },
+				new Event { Name = "Literature Reading", Department = "Arts", EventType = "Reading", Date = new DateTime(2024, 9, 25) },
+				new Event { Name = "Mathematics Olympiad", Department = "Science", EventType = "Competition", Date = new DateTime(2024, 9, 30) },
+				new Event { Name = "Networking Mixer", Department = "Business", EventType = "Mixer", Date = new DateTime(2024, 10, 2) },
+				new Event { Name = "Music Festival", Department = "Arts", EventType = "Festival", Date = new DateTime(2024, 10, 7) },
+				new Event { Name = "Innovation Workshop", Department = "Engineering", EventType = "Workshop", Date = new DateTime(2024, 10, 12) },
+				new Event { Name = "Health Fair", Department = "Health", EventType = "Fair", Date = new DateTime(2024, 10, 18) },
+				new Event { Name = "Career Development Seminar", Department = "Business", EventType = "Seminar", Date = new DateTime(2024, 10, 23) },
+				new Event { Name = "Art Exhibition", Department = "Arts", EventType = "Exhibition", Date = new DateTime(2024, 11, 1) },
 				new Event { Name = "Entrepreneurship Panel", Department = "Business", EventType = "Panel", Date = new DateTime(2024, 11, 10) }
 			};
 			_filteredEvents = new ObservableCollection<Event>(Events);
